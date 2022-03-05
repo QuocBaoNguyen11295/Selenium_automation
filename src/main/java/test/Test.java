089f72e169353a9e246187aa3ca6c0f18c159924
QuocBaoNguyen11295 <nqb11295@gmail.com>
2022-03-05 20:19:27 +0700

    wdm

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -3,9 +3,11 @@ package test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+
 public class Test {
+
     public static void main(String[] args){
-        System.setProperty("webdriver.chrome.driver","driver/chromedriver.exe");
+        WebDriverManager.chromedriver.setup();
         WebDriver driver = new ChromeDriver();
         driver.get("https://demo.guru99.com/v4/");
         driver.close();
